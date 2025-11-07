16dd595e3d14f3e2d622e3c6a56bd811e4fe7781
Bati <no-reply@batijs.dev>
2025-11-07 16:50:48 +0100

    scaffold Vike app with Bati

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { telefunc } from "telefunc/vite";
+import react from "@vitejs/plugin-react";
+import { compiled } from "vite-plugin-compiled-react";
+import vike from "vike/plugin";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  plugins: [
+    vike(),
+    compiled({
+      extract: true,
+    }),
+    react(),
+    telefunc(),
+  ],
+  build: {
+    target: "es2022",
+  },
+});
